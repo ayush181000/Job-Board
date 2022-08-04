3843e6e5a6deacce022422c987b7332a7cd1eecd
Ayush Garg <ayush181000@gmail.com>
2022-08-04 07:33:34 +0530

    deployed

---
+++ README.md
@@ -0,0 +1,8 @@
+#Job Board GraphQL
+
+###[Deployed Link](https://job-board-ayush.herokuapp.com/)
+
+###Sample User Login Id's
+
+**_ID - 1 : alice@facegle.io , Pass : alice123_**
+**_ID - 2 : bob@goobook.co , Pass : bob123_**
--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ const expressJwt = require('express-jwt');
 const jwt = require('jsonwebtoken');
 const db = require('./db');
 
-const port = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
 const app = express();
@@ -53,5 +52,7 @@ app.post('/login', (req, res) => {
   const token = jwt.sign({ sub: user.id }, jwtSecret);
   res.send({ token });
 });
+
+const port = process.env.PORT || 9000;
 
 app.listen(port, () => console.info(`Server started on port ${port}`));
