e38cc82829d43791ea38f490a7844af1442d1d45
Ayush Garg <ayush181000@gmail.com>
2022-08-04 06:31:44 +0530

    final commit

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,50 +1,31 @@
 import { getAccessToken, isLoggedIn } from './auth';
+import gql from 'graphql-tag';
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+} from 'apollo-boost';
 
 const endpointURL = 'http://localhost:9000/graphql';
 
-async function graphqlRequest(query, variables = {}) {
-  const request = {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({ query, variables }),
-  };
-
+const authLink = new ApolloLink((operation, forward) => {
   if (isLoggedIn()) {
-    request.headers['authorization'] = 'Bearer ' + getAccessToken();
+    operation.setContext({
+      headers: { authorization: 'Bearer ' + getAccessToken() },
+    });
   }
 
-  const response = await fetch(endpointURL, request);
+  return forward(operation);
+});
 
-  const responseBody = await response.json();
-  if (responseBody.errors) {
-    const message = responseBody.errors
-      .map((error) => error.message)
-      .join('\n');
-    throw new Error(message);
-  }
-  return responseBody.data;
-}
+const client = new ApolloClient({
+  link: ApolloLink.from([authLink, new HttpLink({ uri: endpointURL })]),
+  cache: new InMemoryCache(),
+});
 
-export async function loadJobs() {
-  const query = `{
-        jobs {
-            id
-            title
-            company {
-                id
-                name
-            }
-        }
-    }`;
-  const { jobs } = await graphqlRequest(query);
-  return jobs;
-}
-
-export async function loadJob(jobId) {
-  const query = `query JobQuery($id: ID!){
-  job(id: $id) {
+const jobDetailFragment = gql`
+  fragment JobDetail on Job {
     id
     title
     company {
@@ -53,37 +34,113 @@ export async function loadJob(jobId) {
     }
     description
   }
-}`;
+`;
 
-  const { job } = await graphqlRequest(query, { id: jobId });
-  return job;
-}
+const createJobMutation = gql`
+  mutation CreateJob($input: CreateJobInput) {
+    job: createJob(input: $input) {
+      ...JobDetail
+    }
+  }
+  ${jobDetailFragment}
+`;
 
-export async function createJob(input) {
-  const mutation = `mutation CreateJob( $input:CreateJobInput){
-  job : createJob(input: $input)
-    {
+const companyQuery = gql`
+  query CompanyQuery($id: ID!) {
+    company(id: $id) {
+      id
+      name
+      description
+      jobs {
+        id
+        title
+      }
+    }
+  }
+`;
+
+const jobQuery = gql`
+  query JobQuery($id: ID!) {
+    job(id: $id) {
+      ...JobDetail
+    }
+  }
+  ${jobDetailFragment}
+`;
+
+const jobsQuery = gql`
+  query JobsQuery {
+    jobs {
       id
       title
+      company {
+        id
+        name
+      }
     }
-}`;
-  const { job } = await graphqlRequest(mutation, { input });
+  }
+`;
+
+export async function createJob(input) {
+  const {
+    data: { job },
+  } = await client.mutate({
+    mutation: createJobMutation,
+    variables: { input },
+    update: (cache, { data }) => {
+      cache.writeQuery({
+        query: jobQuery,
+        variables: { id: data.job.id },
+        data: data,
+      });
+    },
+  });
   return job;
 }
 
 export async function loadCompany(companyId) {
-  const query = `query CompanyQuery( $id: ID!){
-  company(id: $id) {
-    id
-    name
-    description
-    jobs{
-      id
-      title
-    }
-  }
-}`;
-
-  const { company } = await graphqlRequest(query, { id: companyId });
+  const {
+    data: { company },
+  } = await client.query({ query: companyQuery, variables: { id: companyId } });
   return company;
 }
+
+export async function loadJob(jobId) {
+  const {
+    data: { job },
+  } = await client.query({ query: jobQuery, variables: { id: jobId } });
+  return job;
+}
+
+export async function loadJobs() {
+  const { data } = await client.query({
+    query: jobsQuery,
+    fetchPolicy: 'no-cache',
+  });
+  return data.jobs;
+}
+
+//   async function graphqlRequest(query, variables = {}) {
+//   const request = {
+//     method: 'POST',
+//     headers: {
+//       'content-type': 'application/json',
+//     },
+//     body: JSON.stringify({ query, variables }),
+//   };
+
+//   if (isLoggedIn()) {
+//     request.headers['authorization'] = 'Bearer ' + getAccessToken();
+//   }
+
+//   const response = await fetch(endpointURL, request);
+
+//   const responseBody = await response.json();
+//   if (responseBody.errors) {
+//     const message = responseBody.errors
+//       .map((error) => error.message)
+//       .join('\n');
+//     throw new Error(message);
+//   }
+//   return responseBody.data;
+// }
